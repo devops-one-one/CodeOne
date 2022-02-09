3c62af3fe562bc9ef13e32847746f4930116bf9b
Montekar <mantas.pasakarnis@gmail.com>
2022-02-09 16:18:44 +0100

    Design V1

--- gym-one-fr/src/app/app-routing.module.ts
+++ gym-one-fr/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {LiftsComponent} from "./lifts/lifts.component";
+import {BodyComponent} from "./body/body.component";
+import {LoginComponent} from "./login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'lifts', component: LiftsComponent },
+  { path: 'body', component: BodyComponent },
+  { path: 'login', component: LoginComponent },
+
+  { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- gym-one-fr/src/app/app.module.ts
+++ gym-one-fr/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { LiftsComponent } from './lifts/lifts.component';
+import { BodyComponent } from './body/body.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    LiftsComponent,
+    BodyComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
