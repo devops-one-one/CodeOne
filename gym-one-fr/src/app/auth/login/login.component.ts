7a13f45bf6a86a247e64c66b7170bab347274cb8
soma199 <70133057+Laszlo199@users.noreply.github.com>
2022-02-09 22:19:59 +0100

    Create login function and login service and login dto/token

--- gym-one-fr/src/app/auth/login/login.component.ts
+++ gym-one-fr/src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, Validators} from '@angular/forms';
+import {AuthService} from "../shared/auth.service";
+import {LoginDto} from "../shared/login.dto";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm = this._fb.group({
+
+    username: new FormControl(
+      '',
+      [
+        Validators.required,
+      ]
+    ),
+    password: new FormControl(
+      '',
+      Validators.required
+    ),
+    errorText:['']
+  });
+
+  constructor(private _fb: FormBuilder,
+              private _auth: AuthService,) {}
 
   ngOnInit(): void {
   }
+
+
+  login() {
+    const loginDto = this.loginForm.value as LoginDto;
+    this._auth.login(loginDto)
+      .subscribe((token) =>{
+        console.log('Test ');
+        if (token&& token.jwt != null){
+          console.log('Token: ',token.jwt);
+        }else if (token && token.jwt == null){
+          console.log('No token');
+        }
+      });
+  }
+
+  get username() {return this.loginForm.get('username')}
+  get password() {return this.loginForm.get('password')}
 
 }
