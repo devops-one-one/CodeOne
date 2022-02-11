f8fb4c1723d12f774517cbee12f544a409b8d653
Montekar <mantas.pasakarnis@gmail.com>
2022-02-11 16:43:42 +0100

    Design V1.2 login1

--- gym-one-fr/src/app/app.module.ts
+++ gym-one-fr/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { LiftsComponent } from './lifts/lifts.component';
 import { BodyComponent } from './body/body.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { BodyComponent } from './body/body.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
