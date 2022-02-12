9e3ac5671d47e69311621597fd3a7c7a8caf09fc
Faustas Anulis <45820787+faustis1337@users.noreply.github.com>
2022-02-12 12:44:50 +0100

    date

--- gym-one-fr/src/app/app-routing.module.ts
+++ gym-one-fr/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {LiftsComponent} from "./lifts/lifts.component";
 import {BodyComponent} from "./body/body.component";
+import {ExercisesComponent} from "./exercises/exercises.component";
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'lifts', component: LiftsComponent },
   { path: 'body', component: BodyComponent },
+  {path: 'exercises', component: ExercisesComponent},
   {
     path: "auth",
     loadChildren: () => import('./auth/auth.module')
---
+++ gym-one-fr/src/app/exercises/exercises.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercises',
+  templateUrl: './exercises.component.html',
+  styleUrls: ['./exercises.component.scss']
+})
+export class ExercisesComponent implements OnInit {
+  inputDate: any;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    this.inputDate = `${yyyy}-${mm}-${dd}`;
+  }
+
+}
