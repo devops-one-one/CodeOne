512f4950d53cfebd3802a51d409f063e7313592c
Danylo <danyloboshagora@gmail.com>
2022-02-14 12:26:22 +0000

    changed entity id names to simple id

--- GymOneBackend/GymOneBackend.Repository/Entities/ExerciseEntity.cs
+++ GymOneBackend/GymOneBackend.Repository/Entities/ExerciseEntity.cs
@@ -2,7 +2,7 @@ namespace GymOneBackend.Repository.Entities
 {
   public class ExerciseEntity
   {
-    public int ExerciseId { get; set; }
+    public int Id { get; set; }
     public string ExerciseName { get; set; }
     public MuscleGroupEntity MuscleGroupEntity { get; set; }
     public int MuscleGroupEntityId { get; set; }
--- GymOneBackend/GymOneBackend.Repository/Entities/ExerciseSetEntity.cs
+++ GymOneBackend/GymOneBackend.Repository/Entities/ExerciseSetEntity.cs
@@ -5,7 +5,7 @@ namespace GymOneBackend.Repository.Entities
 {
   public class ExerciseSetEntity
   {
-    public int SetId { get; set; }
+    public int Id { get; set; }
     public int UserId { get; set; }
     public ExerciseEntity Exercise { get; set; }
     public int ExerciseId { get; set; }
