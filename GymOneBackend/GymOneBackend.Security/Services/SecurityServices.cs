038c75b967d1319a9b61ad8295439d12c6b76d6d
soma199 <70133057+Laszlo199@users.noreply.github.com>
2022-02-14 15:16:18 +0100

    create the generate token in services

--- GymOneBackend/GymOneBackend.Security/Services/SecurityServices.cs
+++ GymOneBackend/GymOneBackend.Security/Services/SecurityServices.cs
@@ -1,9 +1,14 @@
-﻿using System.Linq;
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
 using GymOneBackend.Core.Model;
 using GymOneBackend.Domain.IRepositories;
 using GymOneBackend.Security.Helpers;
 using GymOneBackend.Security.IServices;
 using GymOneBackend.Security.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace GymOneBackend.Security.Services
 {
@@ -12,9 +17,54 @@ namespace GymOneBackend.Security.Services
         private readonly IUserRepository _repo;
         private readonly IAuthenticationHelper _authenticationHelper;
         
+        
+        public SecurityServices(IConfiguration configuration, 
+            IUserRepository repository,  IAuthenticationHelper authenticationHelper)
+        {
+            Configuration = configuration;
+            _repo = repository;
+            _authenticationHelper = authenticationHelper;
+        }
+        
+        public IConfiguration Configuration { get; }
+        
         public JwtToken GenerateJwtToken(string email, string password, out int userId)
         {
-            throw new System.NotImplementedException();
+            userId = -1;
+            var user = _repo.GetAll().FirstOrDefault(user => user.Email.Equals(email));
+            if(user == null)
+                return new JwtToken()
+                {
+                    Message = "User or password not correct"
+                };
+
+            
+            //Did we not find a user with the given username?
+            if (_authenticationHelper.VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt)
+               )
+            {
+                var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                    Configuration["Jwt:Secret"]));
+                var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+                var token = new JwtSecurityToken(Configuration["Jwt:Issuer"],
+                    Configuration["Jwt:Audience"],
+                    null,
+                    expires: DateTime.Now.AddMinutes(120),
+                    signingCredentials: credentials
+                );
+                userId = user.Id;
+                return new JwtToken(
+                )
+                {
+                    Jwt = new JwtSecurityTokenHandler().WriteToken(token),
+                    Message = "ok"
+                };
+            }
+            //dont need else
+            return new JwtToken()
+            {
+                Message = "User or password not correct"
+            };
         }
 
         public bool Create(string loginDtoEmail, string loginDtoPassword)
