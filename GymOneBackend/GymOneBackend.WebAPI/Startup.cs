981fb4c446afe6eff8c81eacbbab7629a077ff6a
soma199 <70133057+Laszlo199@users.noreply.github.com>
2022-02-14 14:47:26 +0100

    setup bearer token in the startup

--- GymOneBackend/GymOneBackend.WebAPI/Startup.cs
+++ GymOneBackend/GymOneBackend.WebAPI/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using GymOneBackend.Core.IServices;
 using GymOneBackend.Domain.IRepositories;
@@ -12,6 +13,7 @@ using GymOneBackend.Security.Helpers;
 using GymOneBackend.Security.IServices;
 using GymOneBackend.Security.Repositories;
 using GymOneBackend.Security.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -21,6 +23,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace GymOneBackend.WebAPI
@@ -42,6 +45,30 @@ namespace GymOneBackend.WebAPI
       {
         c.SwaggerDoc("v1",
           new OpenApiInfo {Title = "GymOneBackend.WebAPI", Version = "v1"});
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+        {
+          Name = "Authorization",
+          Type = SecuritySchemeType.ApiKey,
+          Scheme = "Bearer",
+          BearerFormat = "JWT",
+          In = ParameterLocation.Header,
+          Description = "JWT Authorization header using Bearer scheme"
+        });
+
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+          {
+            new OpenApiSecurityScheme
+            {
+              Reference = new OpenApiReference
+              {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+              }
+            },
+            new string[] { }
+          }
+        });
       });
       
       services.AddScoped<IExerciseService, ExerciseService>();
@@ -50,7 +77,29 @@ namespace GymOneBackend.WebAPI
       services.AddScoped<ISetExerciseRepository, SetExerciseRepository>();
 
       services.AddDbContext<MainDBContext>(options => { options.UseSqlite("Data Source=main.db"); });
-
+      
+      services.AddAuthentication(authenticationOptions =>
+        {
+          authenticationOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+          authenticationOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+          options.TokenValidationParameters = new TokenValidationParameters
+          {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey =
+              new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(Configuration["Jwt:Secret"])),
+            ValidateIssuer = true,
+            ValidIssuer = Configuration["Jwt:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = Configuration["Jwt:Audience"],
+            ValidateLifetime = true
+          };
+        });
+      
+      //Setting up DI for Security
       services.AddScoped<ISecurityServices, SecurityServices>();
       services.AddScoped<IUserRepository, UserRepository>();
       services.AddScoped<IAuthenticationHelper, AuthenticationHelper>();
