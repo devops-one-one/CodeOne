2d3022f5971f0705a168a8118764fa33af2cc985
soma199 <70133057+Laszlo199@users.noreply.github.com>
2022-03-18 11:27:39 +0100

    create register in module

--- gym-one-fr/src/app/auth/register/register.component.ts
+++ gym-one-fr/src/app/auth/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginDto} from "../shared/login.dto";
+import {FormBuilder, FormControl, Validators} from "@angular/forms";
+import {AuthService} from "../shared/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm = this.fb.group({
+    email: new FormControl(
+      '',
+      [
+        Validators.required,
+        Validators.maxLength(20)
+      ]
+    ),
+    password: new FormControl(
+      '',
+      [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20)
+      ]
+    ),
+  });
+
+  constructor(private fb: FormBuilder,
+              private _auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  register() {
+    const registerDto = this.registerForm.value as LoginDto;
+    this._auth.register(registerDto).subscribe(tokenDto=> {
+      this.router.navigateByUrl("collections")
+    });
+  }
+
+
+  get email() {return this.registerForm.get('email')}
+  get password() {return this.registerForm.get('password')}
 }
