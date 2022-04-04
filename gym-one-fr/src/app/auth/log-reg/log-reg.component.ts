0a1de63be3968824096f207d4afe601f6c497511
soma199 <70133057+Laszlo199@users.noreply.github.com>
2022-04-04 18:07:27 +0200

    move the login and reg in new view

--- gym-one-fr/src/app/auth/log-reg/log-reg.component.ts
+++ gym-one-fr/src/app/auth/log-reg/log-reg.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginDto} from "../shared/login.dto";
+import {FormBuilder, FormControl, Validators} from "@angular/forms";
+import {AuthService} from "../shared/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-log-reg',
@@ -8,7 +12,42 @@ import { Component, OnInit } from '@angular/core';
 export class LogRegComponent implements OnInit {
   isFavorite = false;
 
-  constructor() { }
+  loginForm = this._fb.group({
+
+    email: new FormControl(
+      '',
+      [
+        Validators.required,
+      ]
+    ),
+    password: new FormControl(
+      '',
+      Validators.required
+    ),
+    errorText:['']
+  });
+
+  registerForm = this._fb.group({
+    email: new FormControl(
+      '',
+      [
+        Validators.required,
+        Validators.maxLength(20)
+      ]
+    ),
+    password: new FormControl(
+      '',
+      [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20)
+      ]
+    ),
+  });
+
+  constructor(private _fb: FormBuilder,
+              private _auth: AuthService,
+              private router: Router,) {}
 
   ngOnInit(): void {
   }
@@ -21,4 +60,27 @@ export class LogRegComponent implements OnInit {
       this.isFavorite = true;
     }
   }
+
+  login() {
+    const loginDto = this.loginForm.value as LoginDto;
+    this._auth.login(loginDto)
+      .subscribe((token) =>{
+        console.log('Test ');
+        if (token&& token.jwt != null){
+          console.log('Token: ',token.jwt);
+        }else if (token && token.jwt == null){
+          console.log('No token');
+        }
+      });
+  }
+
+  register() {
+    const registerDto = this.registerForm.value as LoginDto;
+    this._auth.register(registerDto).subscribe(tokenDto=> {
+      this.router.navigateByUrl("Feed me with some direction")
+    });
+  }
+
+  get email() {return this.loginForm.get('email')}
+  get password() {return this.loginForm.get('password')}
 }
